dd609404e91896e37fb09afc18afd538f560c1d1
Harish Kabra <harishkabra@gmail.com>
2025-03-05 11:55:23 -0500

    new commit

--- provider.tf
+++ provider.tf
@@ -2,14 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-       }
+    }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-
-#   assume_role {
-#     role_arn = "arn:aws:iam::820242924248:role/terraform-clould-role-test"
-#   }
 }
